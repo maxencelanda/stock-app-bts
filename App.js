39d5f427e662ad544711cefb0598488414889cce
max <maxence.landa@gmail.com>
2024-03-11 16:23:24 +0100

    expo update + home when logged

--- App.js
+++ App.js
@@ -5,15 +5,31 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Login from './screens/Login';
 import Home from './screens/Home';
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { firebase_auth } from './firebase';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(firebase_auth, (user) => {
+      console.log("user", user);
+      setUser(user);
+    });
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator initialRouteName="Login">       
+        {user ? 
+          <Stack.Screen name="Login" component={Login} />
+        :
+          <Stack.Screen name="Home" component={Home} />
+        }
       </Stack.Navigator>
     </NavigationContainer>
   );
