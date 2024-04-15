9dbb9adff8c430a2b7b3fb4ae0d0f832ac205d75
Vladimir <v.boussekeyt@ecoles-epsi.net>
2024-04-15 16:41:35 +0200

    feat: modal done only missing link to delete and modify

--- screens/Home.js
+++ screens/Home.js
@@ -1,25 +1,24 @@
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
-import { auth } from '../firebase';
-import { platCollection } from '../firestore';
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { auth } from "../firebase";
+import { platCollection } from "../firestore";
 
 export default function Home({ userEmail, navigation }) {
-
-  console.log("user received: " + userEmail)
+  console.log("user received: " + userEmail);
 
   return (
     <View>
       <Text style={styles.title}>ACME STOCK</Text>
       <Text>Logged in as {userEmail}</Text>
       <Text>{platCollection}</Text>
-      <Button title='Go to Read' onPress={() => navigation.navigate('Read')} />
+      <Button title="Go to Read" onPress={() => navigation.navigate("Read")} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   title: {
-    backgroundColor: '#F0F6CA',
-    textAlign: 'center',
+    backgroundColor: "#F0F6CA",
+    textAlign: "center",
     paddingTop: 15,
     paddingBottom: 15,
     fontSize: 18,
--- screens/Read.js
+++ screens/Read.js
@@ -1,119 +1,145 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
-  Button,
-  FlatList,
-  Text,
-  View,
-  StyleSheet,
-  Pressable,
+	Button,
+	FlatList,
+	Text,
+	View,
+	StyleSheet,
+	Pressable,
+	Alert,
+	Modal,
 } from "react-native";
 
 const Read = ({ navigation }) => {
-  const Data = [
-    {
-      id: "1",
-      categorie: "Mexique",
-      nom: "Tacos",
-      quantite: 10,
-      prix: 5,
-    },
-    {
-      id: "2",
-      categorie: "USA",
-      nom: "Hamburger",
-      quantite: 5,
-      prix: 10,
-    },
-  ];
-  const [produit, setProduit] = useState(Data);
-  const [modalVisible, setModalVisible] = useState(false);
+	const Data = [
+		{
+			id: "1",
+			categorie: "Mexique",
+			nom: "Tacos",
+			quantite: 10,
+			prix: 5,
+		},
+		{
+			id: "2",
+			categorie: "USA",
+			nom: "Hamburger",
+			quantite: 5,
+			prix: 10,
+		},
+	];
+	const [produit, setProduit] = useState(Data);
+	const [modalVisible, setModalVisible] = useState(false);
 
-  const Details = () => {
-    return (
-      <View style={styles.container}>
-        <Text>ID</Text>
-        <Text>Categorie</Text>
-        <Text>Produit</Text>
-        <Text>Quantité</Text>
-        <Text>Prix</Text>
-        <Text>Action</Text>
-      </View>
-    );
-  };
-  const Modal = () => {
-    return (
-      <View>
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            Alert.alert("Modal has been closed.");
-            setModalVisible(!modalVisible);
-          }}
-        >
-          <View>
-            <View>
-              <Text>Hello World!</Text>
-              <Pressable onPress={() => setModalVisible(!modalVisible)}>
-                <Text style={styles.textStyle}>Hide Modal</Text>
-              </Pressable>
-            </View>
-          </View>
-        </Modal>
-        <Pressable onPress={() => setModalVisible(true)}>
-          <Text>Show Modal</Text>
-        </Pressable>
-      </View>
-    );
-  };
-  w;
-  const Produits = ({ id, nom, categorie, quantite, prix }) => {
-    return (
-      <View style={styles.container}>
-        <Text>{id}</Text>
-        <Text>{categorie}</Text>
-        <Text>{nom}</Text>
-        <Text>{quantite}</Text>
-        <Text>{prix}</Text>
-        <Button title={"---"}></Button>
-      </View>
-    );
-  };
-  return (
-    <View>
-      <Button
-        title="Add in stock"
-        onPress={() => navigation.navigate("Create")}
-      />
-      <Details />
-      <FlatList
-        data={produit}
-        renderItem={({ item }) => (
-          <Produits
-            id={item.id}
-            categorie={item.categorie}
-            nom={item.nom}
-            quantite={!null ? item.quantite : 0}
-            prix={!null ? item.prix : 0}
-          />
-        )}
-        keyExtractor={(item) => item.id}
-      ></FlatList>
-    </View>
-  );
+	const Details = () => {
+		return (
+			<View style={style.produits}>
+				<Text style={{ padding: 5 }}>ID</Text>
+				<Text style={{ padding: 5 }}>Categorie</Text>
+				<Text style={{ padding: 5 }}>Produit</Text>
+				<Text style={{ padding: 5 }}>Quantité</Text>
+				<Text style={{ padding: 5 }}>Prix</Text>
+				<Text style={{ padding: 5 }}>Action</Text>
+			</View>
+		);
+	};
+	const Produits = ({ id, nom, categorie, quantite, prix }) => {
+		return (
+			<View style={style.produits}>
+				<Text style={{ padding: 5 }}>{id}</Text>
+				<Text style={{ padding: 5 }}>{categorie}</Text>
+				<Text style={{ padding: 5 }}>{nom}</Text>
+				<Text style={{ padding: 5}}>{quantite}</Text>
+				<Text style={{ padding: 5 }}>{prix}</Text>
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={modalVisible}
+					onRequestClose={() => {
+						Alert.alert("Modal has been closed.");
+						setModalVisible(!modalVisible);
+					}}
+				>
+					<View style={style.modalView}>
+						<Text>Modifier ou Supprimer :{id}</Text>
+						<Pressable style={style.button} onPress={() => setModalVisible(!modalVisible)}>
+							<Text style={style.textStyle}>Modifier</Text>
+						</Pressable>
+						<Pressable onPress={() => navigation.navigate("Home")} style={style.button}>
+							<Text style={style.textStyle}>Supprimer</Text>
+						</Pressable>
+					</View>
+				</Modal>
+				<Pressable onPress={() => setModalVisible(true)}>
+					<Text style={{ padding: 5 }}>Details</Text>
+				</Pressable>
+			</View>
+		);
+	};
+	return (
+		<View style={style.container}>
+			<Button
+				title="Add in stock"
+				onPress={() => navigation.navigate("Create")}
+			/>
+			<Details />
+			<FlatList
+				data={produit}
+				renderItem={({ item }) => (
+					<Produits
+						id={item.id}
+						categorie={item.categorie}
+						nom={item.nom}
+						quantite={item.quantite}
+						prix={item.prix}
+					/>
+				)}
+				keyExtractor={(item) => item.id}
+			></FlatList>
+		</View>
+	);
 };
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center", //Centered horizontally
-    flexDirection: "row",
-    justifyContent: "space-between", //Centered vertically
-    borderWidth: 1,
-    borderColor: "black",
-  },
-  text: {
-    padding: 5,
-  },
+const style = StyleSheet.create({
+	container: {
+		alignItems: "center",
+		justifyContent: "center",
+		width: "100%",
+	},
+	produits: {
+		width: 380,
+		flexDirection: "row",
+		justifyContent: "space-around",
+		borderBottomWidth: 1,
+		padding:3,
+	},
+	//modal
+	modalView: {
+		margin: 20,
+		backgroundColor: "white",
+		borderRadius: 20,
+		padding: 35,
+		alignItems: "center",
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5
+	},
+	textStyle: {
+		color: "white",
+		fontWeight: "bold",
+		textAlign: "center"
+	},
+	button: {
+		borderRadius: 20,
+		padding: 10,
+		elevation: 2,
+		backgroundColor: "#2196F3", // Change this to your preferred color
+	},
+	buttonClose: {
+		backgroundColor: "#2196F3", // Change this to your preferred color
+	},
 });
-
 export default Read;
