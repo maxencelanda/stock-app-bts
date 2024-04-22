8ccdc60dabbe9716038bb6c28bf6fe11b7777120
Vladimir <v.boussekeyt@ecoles-epsi.net>
2024-04-22 03:00:38 +0200

    feat: CreateProduct is DONE (minor bug)

--- screens/ModifyProduct.js
+++ screens/ModifyProduct.js
@@ -1,23 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {
-	View,
-	Text,
-	TextInput,
-	Button,
-	FlatList,
-	TouchableOpacity,
-	StyleSheet,
-	TouchableWithoutFeedback, Keyboard
-} from 'react-native';
+import {Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
 import {SelectList} from "react-native-dropdown-select-list";
+
 const axios = require('axios').default;
 const apiUrl = process.env.EXPO_PUBLIC_API_URL
 
-const ModifyProduct = ({route,navigation}) => {
+const ModifyProduct = ({route, navigation}) => {
 	const {id} = route.params;
 	const [Produit, setProduit] = useState();
 	const [Name, setName] = useState('')
-	const [CategoryName, setCategoryName]= useState('')
+	const [CategoryName, setCategoryName] = useState('')
 	const [Price, setPrice] = useState()
 	const [Quantity, setQuantity] = useState()
 	const [Envoi, setEnvoi] = useState(false)
@@ -27,32 +19,32 @@ const ModifyProduct = ({route,navigation}) => {
 
 
 // fetch les categories
-	useEffect(()=>{
-		if(FetchCategory){
-			axios.get(apiUrl+'/category')
-			.then(function(response){
+	useEffect(() => {
+		if (FetchCategory) {
+			axios.get(apiUrl + '/category')
+			.then(function (response) {
 					setCategory(response.data.map(category => category.name))
 				}
-			).catch(function(error){
+			).catch(function (error) {
 				console.log(error)
 			})
 			setFetchCategory(false)
 		}
-	},[FetchCategory])
+	}, [FetchCategory])
 
 // fetch le product
-	useEffect(()=>{
-		if(FetchProduct){
-			axios.get(apiUrl+'/product/'+id)
-			.then(function(response){
+	useEffect(() => {
+		if (FetchProduct) {
+			axios.get(apiUrl + '/product/' + id)
+			.then(function (response) {
 					setProduit(response.data)
 				}
-			).catch(function(error){
+			).catch(function (error) {
 				console.log(error)
 			})
 			setFetchProduct(false)
 		}
-	},[FetchProduct])
+	}, [FetchProduct])
 
 	// Post les données modif
 	useEffect(() => {
@@ -63,13 +55,13 @@ const ModifyProduct = ({route,navigation}) => {
 	}, [Envoi]);
 
 	const sendProductUpdate = () => {
-		axios.post(apiUrl+'/product/edit', {
-			"id" : Produit.id,
+		axios.post(apiUrl + '/product/edit', {
+			"id": Produit.id,
 			"name": Name ? Name : Produit.name,
 			"price": Price ? Price : Produit.price,
 			"quantity": Quantity ? Quantity : Produit.quantity,
-			"idCategory" : {
-				"id" : Produit.idCategory.id,
+			"idCategory": {
+				"id": Produit.idCategory.id,
 				"name": CategoryName ? CategoryName : Category.name,
 			}
 		})
@@ -83,40 +75,40 @@ const ModifyProduct = ({route,navigation}) => {
 
 	return (
 		<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-		<View style={styles.container}>
-			<Text style={styles.title}>Modifier le produit : {id}</Text>
-			<TextInput
-				style={styles.input}
-				placeholder={Produit ? Produit.name : ''}
-				value={Name}
-				onChangeText={(text) => setName(text)}
-			/>
-			<TextInput
-				style={styles.input}
-				placeholder={Produit ? "prix : " +Produit.price.toString() : ''}
-				value={Price}
-				onChangeText={(text) => setPrice(parseInt(text))}
-				keyboardType="numeric"
-			/>
-			<TextInput
-				style={styles.input}
-				placeholder={Produit ? "quantite : "+ Produit.quantity.toString() : ''}
-				value={Quantity}
-				onChangeText={(text) => setQuantity(parseInt(text))}
-				keyboardType="numeric"
-			/>
-			<SelectList
-				setSelected={(val) => setCategoryName(val)}
-				data={Category}
-				save= "value"
-			/>
-			<TouchableOpacity onPress={()=> {
-				setEnvoi(true)
-			}
-			} style={styles.button}>
-				<Text style={{ color: '#fff' }}>Modifier</Text>
-			</TouchableOpacity>
-		</View>
+			<View style={styles.container}>
+				<Text style={styles.title}>Modifier le produit : {id}</Text>
+				<TextInput
+					style={styles.input}
+					placeholder={Produit ? Produit.name : ''}
+					value={Name}
+					onChangeText={(text) => setName(text)}
+				/>
+				<TextInput
+					style={styles.input}
+					placeholder={Produit ? "prix : " + Produit.price.toString() : ''}
+					value={Price}
+					onChangeText={(text) => setPrice(parseInt(text))}
+					keyboardType="numeric"
+				/>
+				<TextInput
+					style={styles.input}
+					placeholder={Produit ? "quantite : " + Produit.quantity.toString() : ''}
+					value={Quantity}
+					onChangeText={(text) => setQuantity(parseInt(text))}
+					keyboardType="numeric"
+				/>
+				<SelectList
+					setSelected={(val) => setCategoryName(val)}
+					data={Category}
+					save="value"
+				/>
+				<TouchableOpacity onPress={() => {
+					setEnvoi(true)
+				}
+				} style={styles.button}>
+					<Text style={{color: '#fff'}}>Modifier</Text>
+				</TouchableOpacity>
+			</View>
 		</TouchableWithoutFeedback>
 	);
 };
